d94bf25299e318329969d7163f4cc1b708bc9200
Kolesnikov, Valentyn <valentyn.kolesnikov@seniordevonly.com>
2016-11-02 13:03:48 +0200

    Add support for the postgre sql.

--- src/test/java/com/github/pt/controller/ShipwreckControllerIntTest.java
+++ src/test/java/com/github/pt/controller/ShipwreckControllerIntTest.java
@@ -12,8 +12,10 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
+@TestPropertySource("/application-test.properties")
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class ShipwreckControllerIntTest {
