9c77e97560944e93299d0633104ff309a439c6d9
Kolesnikov, Valentyn <valentyn.kolesnikov@seniordevonly.com>
2016-11-10 13:38:58 +0200

    Add user ud check while create new workout item report.

--- src/main/java/com/github/pt/reportworkout/ReportWorkoutService.java
+++ src/main/java/com/github/pt/reportworkout/ReportWorkoutService.java
@@ -1,17 +1,22 @@
 package com.github.pt.reportworkout;
 
 import com.github.pt.ResourceNotFoundException;
+import com.github.pt.UnauthorizedException;
 import com.github.pt.programs.InWorkoutItem;
 import com.github.pt.programs.InWorkoutItemReport;
+import com.github.pt.token.InUser;
 import com.github.pt.user.UserService;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 class ReportWorkoutService {
+    private static final Logger LOG = LoggerFactory.getLogger(ReportWorkoutService.class);
     private final InWorkoutItemRepository inWorkoutItemRepository;
     private final InWorkoutItemReportRepository inWorkoutItemReportRepository;
     private final UserService userService;
@@ -34,7 +39,7 @@ class ReportWorkoutService {
 
     WorkoutReportResponseDTO create(String token, WorkoutReportRequestDTO workoutReportRequestDTO) {
         if (!token.isEmpty()) {
-            userService.checkUserToken(token);
+            final InUser inUser = userService.checkUserToken(token).getInUser();
             WorkoutReportResponseDTO workoutReportResponseDTO = new WorkoutReportResponseDTO();
             workoutReportResponseDTO.setItems(new ArrayList<>());
             for (WorkoutItemReportRequestDTO workoutItemReportRequestDTO : workoutReportRequestDTO.getItems()) {
@@ -42,6 +47,12 @@ class ReportWorkoutService {
                 if (inWorkoutItems.isEmpty()) {
                     throw new ResourceNotFoundException("Workout item with id (" + workoutItemReportRequestDTO.getId()
                             + ") not found");
+                }
+                final InWorkoutItem inWorkoutItem = inWorkoutItems.get(inWorkoutItems.size() - 1);
+                if (!inWorkoutItem.getInWorkout().getInProgram().getInUser().getId().equals(inUser.getId())) {
+                    LOG.warn("User with id {} tries to update workout for user with id {}", inUser.getId(),
+                        inWorkoutItem.getInWorkout().getInProgram().getInUser().getId());
+                    throw new UnauthorizedException("This workout item belong to other user");
                 }
                 final InWorkoutItemReport inWorkoutItemReport = new InWorkoutItemReport();
                 inWorkoutItemReport.setInWorkoutItem(inWorkoutItems.get(inWorkoutItems.size() - 1));
