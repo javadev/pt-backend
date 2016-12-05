dec04dca7444530684620792b6f63d0201a78c99
Kolesnikov, Valentyn <valentyn.kolesnikov@seniordevonly.com>
2016-12-05 17:48:54 +0200

    Add more unit tests.

--- src/test/java/com/github/pt/dictionary/DictionaryServiceTest.java
+++ src/test/java/com/github/pt/dictionary/DictionaryServiceTest.java
@@ -21,7 +21,7 @@ public class DictionaryServiceTest {
 
     @Mock
     private DictionaryRepository dictionaryRepository;    
-    
+
     @InjectMocks
     private DictionaryService dictionaryService;
 
@@ -35,7 +35,8 @@ public class DictionaryServiceTest {
     @Test
     public void getNewDictionaryDataKey_wit_key() {
         when(dictionaryRepository.findDictionaryAllValues(eq(DictionaryRepository.ENG_LANGUAGE),
-                eq("exercise_name"), any())).thenReturn(Arrays.asList(new DictionaryData().setDkey("10")));
+                eq("exercise_name"), any())).thenReturn(Arrays.asList(
+                        new DictionaryData().setDkey("10"), new DictionaryData().setDkey("20")));
         dictionaryService.getNewDictionaryDataKey(DictionaryName.exercise_name);
         verify(dictionaryRepository).findDictionaryAllValues(eq(DictionaryRepository.ENG_LANGUAGE),
                 eq("exercise_name"), any());
