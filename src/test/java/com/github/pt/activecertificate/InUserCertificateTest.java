21716a3ae8724c343d7a8d6d31c1e4624429950c
Kolesnikov, Valentyn <valentyn.kolesnikov@seniordevonly.com>
2016-12-05 17:30:05 +0200

    Add more unit tests.

--- src/test/java/com/github/pt/activecertificate/InUserCertificateTest.java
+++ src/test/java/com/github/pt/activecertificate/InUserCertificateTest.java
@@ -1,23 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.github.pt.activecertificate;
 
 import static org.hamcrest.CoreMatchers.notNullValue;
 import org.junit.Test;
 import static org.junit.Assert.assertThat;
 
-/**
- *
- * @author vko
- */
 public class InUserCertificateTest {
     @Test
     public void createAllArgs() {
         assertThat(new InUserCertificate(
                 1L, null, null, null, null), notNullValue());
     }
-    
+
+    @Test
+    public void setters() {
+        InUserCertificate inUserCertificate = new InUserCertificate();
+        inUserCertificate.setInUser(null);
+        assertThat(inUserCertificate, notNullValue());
+    }
+
+    @Test
+    public void getters() {
+        InUserCertificate inUserCertificate = new InUserCertificate();
+        inUserCertificate.getInUser();
+        assertThat(inUserCertificate, notNullValue());
+    }
 }
