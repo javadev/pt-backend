3006adf762870bd1255c899865f4cb70d2061aa2
Kolesnikov, Valentyn <valentyn.kolesnikov@seniordevonly.com>
2016-12-07 14:37:44 +0200

    Add more unit tests.

--- src/test/java/com/github/pt/admin/ptuser/AdminPtUserResourceTest.java
+++ src/test/java/com/github/pt/admin/ptuser/AdminPtUserResourceTest.java
@@ -7,6 +7,7 @@ import org.mockito.Mock;
 import org.junit.runner.RunWith;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyLong;
+import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.verify;
 import org.mockito.runners.MockitoJUnitRunner;
 
@@ -15,9 +16,21 @@ public class AdminPtUserResourceTest {
 
     @Mock
     private AdminPtUserService adminPtUserService;
-    
+
     @InjectMocks
     private AdminPtUserResource adminPtUserResource;
+
+    @Test
+    public void findAll() {
+        adminPtUserResource.findAll();
+        verify(adminPtUserService).findAll();
+    }
+
+    @Test
+    public void findOne() {
+        adminPtUserResource.findOne(1L);
+        verify(adminPtUserService).findOne(eq(1L));
+    }
 
     @Test
     public void create() {
@@ -25,6 +38,13 @@ public class AdminPtUserResourceTest {
         verify(adminPtUserService).create(any(PtUserRequestDTO.class));
     }
 
+    @Test
+    public void update() {
+        adminPtUserResource.update(1L, new PtUserRequestDTO());
+        verify(adminPtUserService).update(eq(1L), any(PtUserRequestDTO.class));
+    }
+
+    @Test
     public void delete() throws Exception {
         adminPtUserResource.delete(1L);
         verify(adminPtUserService).delete(anyLong());
