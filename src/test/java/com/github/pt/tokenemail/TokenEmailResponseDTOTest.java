9df2d2a52a112c36cd942c8beb8dda7a82b11ff4
Kolesnikov, Valentyn <valentyn.kolesnikov@seniordevonly.com>
2016-12-08 15:38:13 +0200

    Add more unit tests.

---
+++ src/test/java/com/github/pt/tokenemail/TokenEmailResponseDTOTest.java
@@ -0,0 +1,33 @@
+package com.github.pt.tokenemail;
+
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+import org.junit.Test;
+
+public class TokenEmailResponseDTOTest {
+    @Test
+    public void createAllArgs() {
+        assertThat(new TokenEmailResponseDTO(null, null), notNullValue());
+    }
+
+    @Test
+    public void createNoArgs() {
+        assertThat(new TokenEmailResponseDTO(), notNullValue());
+    }
+
+    @Test
+    public void setters() {
+        TokenEmailResponseDTO tokenEmailResponseDTO = new TokenEmailResponseDTO();
+        tokenEmailResponseDTO.setToken(null);
+        tokenEmailResponseDTO.setUser(null);
+        assertThat(tokenEmailResponseDTO, notNullValue());
+    }
+
+    @Test
+    public void getters() {
+        TokenEmailResponseDTO tokenEmailResponseDTO = new TokenEmailResponseDTO();
+        tokenEmailResponseDTO.getToken();
+        tokenEmailResponseDTO.getUser();
+        assertThat(tokenEmailResponseDTO, notNullValue());
+    }
+}
---
+++ src/test/java/com/github/pt/tokenemail/UserResponseDTOTest.java
@@ -0,0 +1,32 @@
+package com.github.pt.tokenemail;
+
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+import org.junit.Test;
+
+public class UserResponseDTOTest {
+    @Test
+    public void createAllArgs() {
+        assertThat(new UserResponseDTO(1L, null), notNullValue());
+    }
+
+    @Test
+    public void createNoArgs() {
+        assertThat(new UserResponseDTO(), notNullValue());
+    }
+
+    @Test
+    public void setters() {
+        UserResponseDTO userResponseDTO = new UserResponseDTO();
+        userResponseDTO.setId(1L);
+        userResponseDTO.setName(null);
+        assertThat(userResponseDTO, notNullValue());
+    }
+
+    @Test
+    public void getters() {
+        UserResponseDTO userResponseDTO = new UserResponseDTO();
+        userResponseDTO.getId();
+        userResponseDTO.getName();
+        assertThat(userResponseDTO, notNullValue());
+    }}
