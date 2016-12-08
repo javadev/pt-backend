f6e3d592452cb68cde9633a9aa2f2be2a4410ae2
Kolesnikov, Valentyn <valentyn.kolesnikov@seniordevonly.com>
2016-12-08 13:53:50 +0200

    Add more unit tests.

---
+++ src/test/java/com/github/pt/admin/user/AdminUserProgramResourceTest.java
@@ -0,0 +1,45 @@
+package com.github.pt.admin.user;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyLong;
+import static org.mockito.Matchers.eq;
+import org.mockito.Mock;
+import static org.mockito.Mockito.verify;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AdminUserProgramResourceTest {
+
+    @Mock
+    private AdminUserProgramService userProgramService;
+    @InjectMocks
+    private AdminUserProgramResource adminUserProgramResource;
+
+    @Test
+    public void findOne() {
+        adminUserProgramResource.findOne(1L);
+        verify(userProgramService).findOne(eq(1L));
+    }
+
+    @Test
+    public void create() {
+        adminUserProgramResource.create(new UserProgramRequestDTO());
+        verify(userProgramService).create(any(UserProgramRequestDTO.class));
+    }
+
+    @Test
+    public void update() {
+        adminUserProgramResource.update(1L, new UserProgramRequestDTO());
+        verify(userProgramService).update(eq(1L), any(UserProgramRequestDTO.class));
+    }
+
+    @Test
+    public void delete() {
+        adminUserProgramResource.delete(1L);
+        verify(userProgramService).delete(anyLong());
+    }
+
+}
