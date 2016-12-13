20c45ba0b4b7f5a164a9bb40b598674ecd0e8009
Kolesnikov, Valentyn <valentyn.kolesnikov@seniordevonly.com>
2016-12-13 17:07:57 +0200

    Add more unit tests.

---
+++ src/test/java/com/osomapps/pt/auth/AuthUserResourceTest.java
@@ -0,0 +1,24 @@
+package com.osomapps.pt.auth;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import static org.mockito.Mockito.verify;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AuthUserResourceTest {
+    @Mock
+    private AuthUserService authUserService;    
+    
+    @InjectMocks
+    private AuthUserResource authUserResource;
+
+    @Test
+    public void findOne() {
+        authUserResource.findOne();
+        verify(authUserService).findOne();
+    }
+
+}
---
+++ src/test/java/com/osomapps/pt/auth/AuthUserResourceWebTest.java
@@ -0,0 +1,43 @@
+package com.osomapps.pt.auth;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.TestPropertySource;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@TestPropertySource("/application-test.properties")
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
+public class AuthUserResourceWebTest {
+
+    @Test
+    public void testListAll() throws Exception {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("X-Token", "");
+        TestRestTemplate restTemplate = new TestRestTemplate();
+        ResponseEntity<String> response = restTemplate.exchange(
+            "http://localhost:9090/api/v1/auth/user", HttpMethod.GET, new HttpEntity<>(headers), String.class);
+
+        assertThat(response.getStatusCode(), equalTo(HttpStatus.NOT_FOUND));
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode responseJson = objectMapper.readTree(response.getBody());
+
+        assertThat(responseJson.isMissingNode(), is(false));
+        assertThat(responseJson.toString(), equalTo("{\"error_description\":\"No message available\"}"));
+    }
+}
