c6717362e1fbb5a13f458557ce183c6c399f4353
Kolesnikov, Valentyn <valentyn.kolesnikov@seniordevonly.com>
2016-12-14 15:14:16 +0200

    Add more unit tests.

---
+++ src/test/java/com/osomapps/pt/tokenemail/TokenEmailSignupResourceTest.java
@@ -0,0 +1,37 @@
+package com.osomapps.pt.tokenemail;
+
+import com.osomapps.pt.token.InUser;
+import java.util.List;
+import static org.hamcrest.CoreMatchers.equalTo;
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import org.mockito.Mock;
+import static org.mockito.Mockito.verify;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.mock.web.MockHttpServletRequest;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TokenEmailSignupResourceTest {
+
+    @Mock
+    private TokenEmailSignupService tokenEmailSignupService;
+
+    @InjectMocks
+    private TokenEmailSignupResource tokenEmailSignupResource;
+
+    @Test
+    public void list() {
+        List<InUser> inUsers = tokenEmailSignupResource.list();
+        assertThat(inUsers.isEmpty(), equalTo(true)); 
+    }
+
+    @Test
+    public void create() {
+        tokenEmailSignupResource.create(new TokenEmailSignupRequestDTO(), new MockHttpServletRequest());
+        verify(tokenEmailSignupService).createNewToken(any(TokenEmailSignupRequestDTO.class), anyString());
+    }
+}
--- src/test/java/com/osomapps/pt/tokenemail/TokenEmailSignupServiceTest.java
+++ src/test/java/com/osomapps/pt/tokenemail/TokenEmailSignupServiceTest.java
@@ -8,12 +8,17 @@ import java.util.Arrays;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyObject;
 import static org.mockito.Matchers.anyString;
 import org.mockito.Mock;
+import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.when;
+import org.mockito.invocation.InvocationOnMock;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.validation.Errors;
 
 @RunWith(MockitoJUnitRunner.class)
 public class TokenEmailSignupServiceTest {
@@ -44,12 +49,45 @@ public class TokenEmailSignupServiceTest {
     }
 
     @Test(expected = UnauthorizedException.class)
+    public void createInUserEmail_wrong_email() {
+        doAnswer((Answer<Void>) (InvocationOnMock invocation) -> {
+            Object[] args = invocation.getArguments();
+            ((Errors) args[1]).reject("emailValidator", "emailValidator");
+            return null;
+        }).when(emailValidator).validate(anyObject(), any(Errors.class));
+        tokenEmailSignupService.createInUserEmail(new TokenEmailSignupRequestDTO().setUser(
+                new UserSignupRequestDTO().setEmail("test@mail.com")));
+    }
+
+    @Test(expected = UnauthorizedException.class)
+    public void createInUserEmail_wrong_dataurl() {
+        doAnswer((Answer<Void>) (InvocationOnMock invocation) -> {
+            Object[] args = invocation.getArguments();
+            ((Errors) args[1]).reject("dataurlValidator", "dataurlValidator");
+            return null;
+        }).when(dataurlValidator).validate(anyObject(), any(Errors.class));
+        tokenEmailSignupService.createInUserEmail(new TokenEmailSignupRequestDTO().setUser(
+                new UserSignupRequestDTO().setEmail("test@mail.com")));
+    }
+
+    @Test(expected = UnauthorizedException.class)
+    public void createInUserEmail_wrong_name() {
+        doAnswer((Answer<Void>) (InvocationOnMock invocation) -> {
+            Object[] args = invocation.getArguments();
+            ((Errors) args[1]).reject("dataurlValidator", "dataurlValidator");
+            return null;
+        }).when(nameValidator).validate(anyObject(), any(Errors.class));
+        tokenEmailSignupService.createInUserEmail(new TokenEmailSignupRequestDTO().setUser(
+                new UserSignupRequestDTO().setEmail("test@mail.com")));
+    }
+
+    @Test(expected = UnauthorizedException.class)
     public void createInUserEmail_already_registered() {
         when(inUserEmailRepository.findByLogin(anyString())).thenReturn(Arrays.asList(new InUserEmail()));
         tokenEmailSignupService.createInUserEmail(new TokenEmailSignupRequestDTO().setUser(
                 new UserSignupRequestDTO().setEmail("test@mail.com")));
     }
-    
+
     @Test
     public void createNewToken() {
         when(inUserRepository.save((InUser) anyObject())).thenAnswer(i -> i.getArguments()[0]);
