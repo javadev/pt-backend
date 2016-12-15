8c6701da8901620c0af210065b253c3018aa4c93
Valentyn Kolesnikov <javadev75@gmail.com>
2016-12-15 12:22:01 +0200

    Add more unit tests.

---
+++ src/main/resources/db/migration/V4.45__drop_parse_workout_test_tables.sql
@@ -0,0 +1,4 @@
+DROP TABLE ptcore.program_test CASCADE;
+DROP TABLE ptcore.parse_workout_test CASCADE;
+DROP TABLE ptcore.parse_warmup_workout_item_test CASCADE;
+DROP TABLE ptcore.parse_workout_item_test CASCADE;
--- src/test/java/com/osomapps/pt/goals/GoalServiceTest.java
+++ src/test/java/com/osomapps/pt/goals/GoalServiceTest.java
@@ -2,11 +2,13 @@ package com.osomapps.pt.goals;
 
 import com.osomapps.pt.dictionary.DictionaryService;
 import com.osomapps.pt.user.UserService;
+import java.util.Arrays;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import org.mockito.runners.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -22,7 +24,15 @@ public class GoalServiceTest {
     private GoalService goalService;
 
     @Test
-    public void testSomeMethod() {
+    public void foindAll_empty_token() {
+        goalService.findAll("");
+        verify(goalRepository).findAll();
+    }
+
+    @Test
+    public void foindAll() {
+        when(goalRepository.findAll()).thenReturn(Arrays.asList(
+                new Goal().setGoalParameters(Arrays.asList(new GoalParameter()))));
         goalService.findAll("1");
         verify(goalRepository).findAll();
     }
