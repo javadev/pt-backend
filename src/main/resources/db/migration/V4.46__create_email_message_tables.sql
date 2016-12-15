20a6527610ddb1aba20e5239b2a608c8aa7132f4
Kolesnikov, Valentyn <valentyn.kolesnikov@seniordevonly.com>
2016-12-15 18:11:48 +0200

    Fix email message data tables.

--- src/main/resources/db/migration/V4.46__create_email_message_tables.sql
+++ src/main/resources/db/migration/V4.46__create_email_message_tables.sql
@@ -19,13 +19,13 @@ CREATE TABLE ptcore.email_message_template (
 
 INSERT INTO ptcore.email_message_template (email_message_type_id, d_email_subject, d_email_text) VALUES
 (1, '10', '10'),
-(1, '20', '20');
+(2, '20', '20');
 
 INSERT INTO ptcore.dictionary_data (dname, dkey, dvalue) VALUES
 ('email_subject', '10', 'Welcome to the Personal Trainer Application!'),
 ('email_subject', '20', 'Reset password service');
 
 INSERT INTO ptcore.dictionary_data (dname, dkey, dvalue) VALUES
-('email_text', '10', 'Dear {{user_name}}!\n\nYour registration was completed succesfully. Please click to the link to confirm your e-mail {{email_confirm_link}}.\n\nYours PT support team.'),
+('email_text', '10', 'Dear {{user_name}}!\n\nYour registration was completed successfully. Please click to the link to confirm your e-mail {{email_confirm_link}}.\n\nYours PT support team.'),
 ('email_text', '20', 'Dear {{user_name}}!\n\nPlease click to the link to reset your password {{reset_password_link}}.\n\nYours PT support team.');
 
