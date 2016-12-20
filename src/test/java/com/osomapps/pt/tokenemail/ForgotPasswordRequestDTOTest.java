4ff26d6f4ccdce87cffbf2c5205315dd3daea62f
Kolesnikov, Valentyn <valentyn.kolesnikov@seniordevonly.com>
2016-12-20 16:07:02 +0200

    Add more unit tests.

---
+++ src/test/java/com/osomapps/pt/tokenemail/ForgotPasswordRequestDTOTest.java
@@ -0,0 +1,14 @@
+package com.osomapps.pt.tokenemail;
+
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+import org.junit.Test;
+
+public class ForgotPasswordRequestDTOTest {
+
+    @Test
+    public void allArgsConstructor() {
+        assertThat(new ForgotPasswordRequestDTO(null), notNullValue());
+    }
+
+}
