183370a088563e310249c93123f70fa3f3fa9786
Kolesnikov, Valentyn <valentyn.kolesnikov@seniordevonly.com>
2016-12-23 12:42:54 +0200

    Add more unit tests.

---
+++ src/test/java/com/osomapps/pt/token/InUserGoalTest.java
@@ -0,0 +1,39 @@
+package com.osomapps.pt.token;
+
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+import org.junit.Test;
+
+public class InUserGoalTest {
+    @Test
+    public void createAllArgs() {
+        assertThat(new InUserGoal(
+                1L, null, null, null, null, null, null), notNullValue());
+    }
+
+    @Test
+    public void setters() {
+        InUserGoal inUserGoal = new InUserGoal()
+            .setId(1L)
+            .setCreated(null)
+            .setGoalId(null)
+            .setD_goal_title(null)
+            .setD_goal_title_2(null)
+            .setGoal_value(null)
+            .setInUsers(null);
+        assertThat(inUserGoal, notNullValue());
+    }
+
+    @Test
+    public void getters() {
+        InUserGoal inUserGoal = new InUserGoal();
+        inUserGoal.getId();
+        inUserGoal.getCreated();
+        inUserGoal.getGoalId();
+        inUserGoal.getD_goal_title();
+        inUserGoal.getD_goal_title_2();
+        inUserGoal.getGoal_value();
+        inUserGoal.getInUsers();
+        assertThat(inUserGoal, notNullValue());
+    }
+}
