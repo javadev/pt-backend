f62641d6b43d912a216c897891d97e7491030605
Kolesnikov, Valentyn <valentyn.kolesnikov@seniordevonly.com>
2016-12-23 16:44:09 +0200

    Improve file download.

--- src/main/java/com/osomapps/pt/exercises/ExercisesImageResource.java
+++ src/main/java/com/osomapps/pt/exercises/ExercisesImageResource.java
@@ -3,6 +3,7 @@ package com.osomapps.pt.exercises;
 import java.io.IOException;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.FastByteArrayOutputStream;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,12 +25,15 @@ class ExercisesImageResource {
     @ResponseBody
     Object findOne(@PathVariable Long id, @PathVariable String fileName, HttpServletResponse response)
             throws IOException {
-        final ExerciseImageDTO exerciseImageDTO = exerciseImageService.findOne(id, fileName,
-                response.getOutputStream());
-        response.setContentType(exerciseImageDTO.getFileType());
-        response.setHeader("Content-disposition",
-                "attachment; filename=" + exerciseImageDTO.getFileName());
-        response.getOutputStream().close();
+        try (FastByteArrayOutputStream outputStream = new FastByteArrayOutputStream()) {
+            final ExerciseImageDTO exerciseImageDTO = exerciseImageService.findOne(id, fileName, outputStream);
+            response.setContentType(exerciseImageDTO.getFileType());
+            response.setHeader("Content-disposition",
+                    "attachment; filename=" + exerciseImageDTO.getFileName());
+            outputStream.writeTo(response.getOutputStream());
+            response.getOutputStream().close();
+            outputStream.reset();
+        }
         return null;
     }
 }
