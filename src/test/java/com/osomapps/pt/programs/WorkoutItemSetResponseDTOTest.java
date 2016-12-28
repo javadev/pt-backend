7436e57e4644212c39bbb846a95256571f47220a
Kolesnikov, Valentyn <valentyn.kolesnikov@seniordevonly.com>
2016-12-28 11:43:45 +0200

    Add more unit tests.

---
+++ src/test/java/com/osomapps/pt/programs/WorkoutItemSetResponseDTOTest.java
@@ -0,0 +1,38 @@
+package com.osomapps.pt.programs;
+
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+import org.junit.Test;
+
+public class WorkoutItemSetResponseDTOTest {
+    @Test
+    public void allArgsConstructor() {
+        assertThat(new WorkoutItemSetResponseDTO(1, null, null, null, null, null, null), notNullValue());
+    }
+
+    @Test
+    public void setters() {
+        WorkoutItemSetResponseDTO workoutItemSetResponseDTO = new WorkoutItemSetResponseDTO()
+            .setRepetitions(null)
+            .setWeight(null)
+            .setBodyweight(null)
+            .setTime_in_min(null)
+            .setSpeed(null)
+            .setIncline(null)
+            .setResistance(null);
+        assertThat(workoutItemSetResponseDTO, notNullValue());
+    }
+
+    @Test
+    public void getters() {
+        WorkoutItemSetResponseDTO workoutItemSetResponseDTO = new WorkoutItemSetResponseDTO();
+        workoutItemSetResponseDTO.getRepetitions();
+        workoutItemSetResponseDTO.getWeight();
+        workoutItemSetResponseDTO.getBodyweight();
+        workoutItemSetResponseDTO.getTime_in_min();
+        workoutItemSetResponseDTO.getSpeed();
+        workoutItemSetResponseDTO.getIncline();
+        workoutItemSetResponseDTO.getResistance();
+        assertThat(workoutItemSetResponseDTO, notNullValue());
+    }
+}
---
+++ src/test/java/com/osomapps/pt/programs/WorkoutResponseDTOTest.java
@@ -0,0 +1,32 @@
+package com.osomapps.pt.programs;
+
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+import org.junit.Test;
+
+public class WorkoutResponseDTOTest {
+    @Test
+    public void allArgsConstructor() {
+        assertThat(new WorkoutResponseDTO(1L, null, null, null), notNullValue());
+    }
+
+    @Test
+    public void setters() {
+        WorkoutResponseDTO workoutResponseDTO = new WorkoutResponseDTO()
+            .setId(null)
+            .setName(null)
+            .setWarmup(null)
+            .setItems(null);
+        assertThat(workoutResponseDTO, notNullValue());
+    }
+
+    @Test
+    public void getters() {
+        WorkoutResponseDTO workoutResponseDTO = new WorkoutResponseDTO();
+        workoutResponseDTO.getId();
+        workoutResponseDTO.getName();
+        workoutResponseDTO.getWarmup();
+        workoutResponseDTO.getItems();
+        assertThat(workoutResponseDTO, notNullValue());
+    }
+}
