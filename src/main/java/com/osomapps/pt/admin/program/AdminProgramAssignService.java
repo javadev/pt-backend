0094e788ddd9f26f27c8e07b2d5026a788047845
Kolesnikov, Valentyn <valentyn.kolesnikov@seniordevonly.com>
2017-01-05 16:20:42 +0200

    Improve program generation.

--- src/main/java/com/osomapps/pt/admin/program/AdminProgramAssignService.java
+++ src/main/java/com/osomapps/pt/admin/program/AdminProgramAssignService.java
@@ -11,11 +11,7 @@ import com.osomapps.pt.programs.InWorkout;
 import com.osomapps.pt.programs.InWorkoutItem;
 import com.osomapps.pt.programs.InWorkoutItemSet;
 import com.osomapps.pt.programs.InWorkoutRepository;
-import com.osomapps.pt.programs.ParseGoal;
-import com.osomapps.pt.programs.ParseUserGroup;
-import com.osomapps.pt.programs.ParseWarmupWorkoutItem;
 import com.osomapps.pt.programs.ParseWorkout;
-import com.osomapps.pt.programs.ParseWorkoutItem;
 import com.osomapps.pt.reportworkout.InWorkoutItemRepository;
 import com.osomapps.pt.token.InUser;
 import com.osomapps.pt.token.InUserRepository;
@@ -24,8 +20,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
-import com.osomapps.pt.programs.ParseGoalRepository;
-import com.osomapps.pt.programs.ParsePart;
 import com.osomapps.pt.programs.ParseProgram;
 import com.osomapps.pt.programs.ParseProgramRepository;
 import com.osomapps.pt.programs.ParseRound;
@@ -71,67 +65,8 @@ public class AdminProgramAssignService {
         this.dictionaryService = dictionaryService;
     }
 
-//    private static boolean isNotNullOrEmpty (String str) {
-//        return str != null && !str.isEmpty();
-//    }
-
     public InUser assign(InUser inUser) {
-//        parseGoals.stream().forEachOrdered(parseUser -> {
-//            final List<InUser> inUsers = inUserRepository.findAll();
-//            final List<InUser> inUsersWithName = inUsers.stream().filter(inUser -> 
-//                    getUserName(inUser).orElse("").equals(parseUser.getName())).collect(Collectors.toList());
-//            if (inUsersWithName.isEmpty()) {
-//                parseUser.setErrors(Arrays.asList(parseUser.getErrors(),
-//                    "Cannot assign user " + parseUser.getName() + ". User not found.")
-//                        .stream().filter(AdminProgramAssignService::isNotNullOrEmpty).collect(Collectors.joining(", ")));
-//            } else if (inUsersWithName.size() > 1) {
-//                parseUser.setErrors(Arrays.asList(parseUser.getErrors(),
-//                    "Cannot assign user " + parseUser.getName() + ". More than one user found.")
-//                        .stream().filter(AdminProgramAssignService::isNotNullOrEmpty).collect(Collectors.joining(", ")));
-//            } else {
-//                parseUser.setIn_user_id(inUsersWithName.get(0).getId());
-//                final InProgram inProgram = new InProgram();
-//                inProgram.setInUser(inUsersWithName.get(0));
-//                inProgram.setName(parseUser.getProgram().getName());
-//                inProgram.setD_program_type("personal");
-//                final InProgram savedInProgram = inProgramRepository.save(inProgram);
-//                for (ParseWorkout parseWorkout : parseUser.getParseWorkouts()) {
-//                    final InWorkout inWorkout = new InWorkout();
-//                    inWorkout.setInProgram(savedInProgram);
-//                    inWorkout.setD_workout_name(parseWorkout.getName());
-//                    final InWorkout savedInWorkout = inWorkoutRepository.save(inWorkout);
-//                    if (!parseWorkout.getParseWarmupWorkoutItems().isEmpty()) {
-//                        final ParseWarmupWorkoutItem parseWarmupWorkoutItem = parseWorkout.getParseWarmupWorkoutItems().get(0);
-//                        final InWarmupWorkoutItem inWarmupWorkoutItem = new InWarmupWorkoutItem();
-//                        inWarmupWorkoutItem.setInWorkout(savedInWorkout);
-//                        inWarmupWorkoutItem.setD_exercise_name(parseWarmupWorkoutItem.getName());
-//                        final Optional<Long> exerciseId = adminProgramScanExerciseService.getExerciseIdByName(
-//                                parseWarmupWorkoutItem.getName());
-//                        inWarmupWorkoutItem.setD_exercise_id("" + exerciseId.orElse(0L));
-//                        inWarmupWorkoutItem.setSpeed(parseWarmupWorkoutItem.getSpeed());
-//                        inWarmupWorkoutItem.setIncline(parseWarmupWorkoutItem.getIncline());
-//                        inWarmupWorkoutItem.setTime_in_min(parseWarmupWorkoutItem.getTime_in_min());
-//                        inWarmupWorkoutItemRepository.save(inWarmupWorkoutItem);
-//                    }
-//                    for (ParseWorkoutItem parseWorkoutItem : parseWorkout.getParseWorkoutItems()) {
-//                        final InWorkoutItem inWorkoutItem = new InWorkoutItem();
-//                        inWorkoutItem.setInWorkout(savedInWorkout);
-//                        inWorkoutItem.setD_exercise_name(parseWorkoutItem.getName());
-//                        final Optional<Long> exerciseId = adminProgramScanExerciseService.getExerciseIdByName(
-//                                parseWorkoutItem.getName());
-//                        inWorkoutItem.setD_exercise_id("" + exerciseId.orElse(0L));
-//                        inWorkoutItem.setSets(parseWorkoutItem.getSets());
-//                        inWorkoutItem.setRepetitions(parseWorkoutItem.getRepetitions());
-//                        inWorkoutItem.setWeight(parseWorkoutItem.getWeight() == null ? null : parseWorkoutItem.getWeight().floatValue());
-//                        inWorkoutItem.setTime_in_min(parseWorkoutItem.getTime_in_min());
-//                        inWorkoutItem.setSpeed(parseWorkoutItem.getSpeed());
-//                        inWorkoutItem.setResistance(parseWorkoutItem.getResistance());
-//                        final InWorkoutItem savedInWorkoutItem = inWorkoutItemRepository.save(inWorkoutItem);
-//                        parseWorkoutItem.setIn_workout_item_id(savedInWorkoutItem.getId());
-//                    }
-//                }
-//            }
-//        });
+
         List<ParseProgram> parsePrograms = parseProgramRepository.findAll(new Sort(Sort.Direction.DESC, "id"));
         if (parsePrograms.isEmpty()) {
             throw new UnauthorizedException("There are no programs found.");
@@ -143,39 +78,39 @@ public class AdminProgramAssignService {
         }
         if (inUser.getInUserGoals().isEmpty()) {
             log.warn("Cannot create program for user without goals.");
-            return inUser;            
+            return inUser;
         }
         List<ParseRound> parseRounds = getRoundsGorGoalAndUserGroup(parsePrograms,
                 inUser.getInUserGoals().get(0), userGroup.get());
         List<ParseWorkout> parseWorkouts = getParseWorkouts(parseRounds);
         final InProgram inProgram = new InProgram()
-                .setName("Test program for user with id " + inUser.getId())
-                .setInWorkouts(parseWorkouts.stream().map(parseWorkout ->
-                    new InWorkout()
+                .setName("Test program for user " + inUser.getId())
+                .setInWorkouts(parseWorkouts.stream().map(parseWorkout
+                        -> new InWorkout()
                         .setD_workout_name(parseWorkout.getName())
-                        .setInWarmupWorkoutItems(parseWorkout.getParseWarmupWorkoutItems().stream().map(parseWarmupWorkoutItem ->
-                                new InWarmupWorkoutItem()
-                                        .setD_exercise_name(parseWarmupWorkoutItem.getName())
-                                        .setTime_in_sec(minToSec(parseWarmupWorkoutItem.getTime_in_min()))
-                                        .setSpeed(parseWarmupWorkoutItem.getSpeed())
-                                        .setIncline(parseWarmupWorkoutItem.getIncline())
+                        .setInWarmupWorkoutItems(parseWorkout.getParseWarmupWorkoutItems().stream().map(parseWarmupWorkoutItem
+                                -> new InWarmupWorkoutItem()
+                                .setD_exercise_name(parseWarmupWorkoutItem.getName())
+                                .setTime_in_sec(minToSec(parseWarmupWorkoutItem.getTime_in_min()))
+                                .setSpeed(parseWarmupWorkoutItem.getSpeed())
+                                .setIncline(parseWarmupWorkoutItem.getIncline())
                         ).collect(Collectors.toList()))
-                        .setInWorkoutItems(parseWorkout.getParseWorkoutItems().stream().map(parseWorkoutItem ->
-                                                new InWorkoutItem()
-                                                        .setD_exercise_name(parseWorkoutItem.getName())
-                                                .setInWorkoutItemSets(parseWorkoutItem.getParseWorkoutItemSets().stream().map(parseWorkoutItemSet ->
-                                                        new InWorkoutItemSet()
-                                                                .setRepetitions(parseWorkoutItemSet.getRepetitions())
-                                                        .setRepetitions_to_failure(parseWorkoutItemSet.getRepetitions_to_failure())
-                                                        .setWeight(parseWorkoutItemSet.getWeight())
-                                                        .setBodyweight(parseWorkoutItemSet.getBodyweight())
-                                                        .setTime_in_sec(minToSec(parseWorkoutItemSet.getTime_in_min()))
-                                                        .setSpeed(parseWorkoutItemSet.getSpeed())
-                                                        .setIncline(parseWorkoutItemSet.getIncline())
-                                                        .setResistance(parseWorkoutItemSet.getResistance())
-                                                ).collect(Collectors.toList()))
-                                                .setInWorkoutItemReports(Collections.emptyList())
-                                        ).collect(Collectors.toList()))).collect(Collectors.toList()));
+                        .setInWorkoutItems(parseWorkout.getParseWorkoutItems().stream().map(parseWorkoutItem
+                                -> new InWorkoutItem()
+                                .setD_exercise_name(parseWorkoutItem.getName())
+                                .setInWorkoutItemSets(parseWorkoutItem.getParseWorkoutItemSets().stream().map(parseWorkoutItemSet
+                                        -> new InWorkoutItemSet()
+                                        .setRepetitions(parseWorkoutItemSet.getRepetitions())
+                                        .setRepetitions_to_failure(parseWorkoutItemSet.getRepetitions_to_failure())
+                                        .setWeight(parseWorkoutItemSet.getWeight())
+                                        .setBodyweight(parseWorkoutItemSet.getBodyweight())
+                                        .setTime_in_sec(minToSec(parseWorkoutItemSet.getTime_in_min()))
+                                        .setSpeed(parseWorkoutItemSet.getSpeed())
+                                        .setIncline(parseWorkoutItemSet.getIncline())
+                                        .setResistance(parseWorkoutItemSet.getResistance())
+                                ).collect(Collectors.toList()))
+                                .setInWorkoutItemReports(Collections.emptyList())
+                        ).collect(Collectors.toList()))).collect(Collectors.toList()));
         inProgram.setInUser(inUser);
         inProgram.getInWorkouts().forEach(inWorkout -> {
             inWorkout.setInProgram(inProgram);
@@ -202,7 +137,7 @@ public class AdminProgramAssignService {
     }
     
     private Integer minToSec(Float value) {
-        return value == null ? null : value.intValue() * 60;
+        return value == null ? null : Float.valueOf(value * 60).intValue();
     }
 
     private List<ParseRound> getRoundsGorGoalAndUserGroup(List<ParseProgram> parsePrograms,
@@ -244,20 +179,6 @@ public class AdminProgramAssignService {
         });
         return parseWorkouts;
     }
-
-//    private Optional<String> getUserName(InUser inUser) {
-//        final Optional<String> userName;
-//        if (inUser.getInUserFacebooks() == null || inUser.getInUserFacebooks().isEmpty()) {
-//            if (inUser.getInUserEmails().isEmpty()) {
-//                userName = Optional.empty();
-//            } else {
-//                userName = Optional.of(inUser.getInUserEmails().get(inUser.getInUserEmails().size() - 1).getUser_name());
-//            }
-//        } else {
-//            userName = Optional.of(inUser.getInUserFacebooks().get(inUser.getInUserFacebooks().size() - 1).getUser_name());
-//        }
-//        return userName;
-//    }
 
     enum Gender {
         male, female, unknown
