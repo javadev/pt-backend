324acd6c13aefd25da79b0dddd254f61ab5e3a7c
Kolesnikov, Valentyn <valentyn.kolesnikov@seniordevonly.com>
2017-01-16 12:40:02 +0200

    Add more unit tests.

--- src/test/java/com/osomapps/pt/reportphoto/ReportPhotoServiceTest.java
+++ src/test/java/com/osomapps/pt/reportphoto/ReportPhotoServiceTest.java
@@ -1,5 +1,6 @@
 package com.osomapps.pt.reportphoto;
 
+import com.osomapps.pt.UnauthorizedException;
 import com.osomapps.pt.reportweight.InUserWeight;
 import com.osomapps.pt.token.InUser;
 import com.osomapps.pt.token.InUserGoal;
@@ -9,7 +10,6 @@ import java.util.Arrays;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
-import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Matchers.eq;
 import org.mockito.Mock;
@@ -20,6 +20,12 @@ import org.mockito.runners.MockitoJUnitRunner;
 import com.osomapps.pt.goals.InUserPhotoRepository;
 import com.osomapps.pt.token.InUserPhoto;
 import com.osomapps.pt.tokenemail.DataurlValidator;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyObject;
+import static org.mockito.Mockito.doAnswer;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.springframework.validation.Errors;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ReportPhotoServiceTest {
@@ -37,7 +43,7 @@ public class ReportPhotoServiceTest {
     @Test
     public void findAll() {
         InUserLogin inUserLogin = new InUserLogin();
-        InUser inUserForLogin = new InUser();
+        InUser inUserForLogin = new InUser().setInUserPhotos(Arrays.asList(new InUserPhoto()));
         inUserForLogin.setId(10L);
         inUserForLogin.setInUserWeights(Arrays.asList(new InUserWeight().setId(1L).setWeight(1F)));
         inUserLogin.setInUser(inUserForLogin);
@@ -56,6 +62,41 @@ public class ReportPhotoServiceTest {
         when(inUserPhotoRepository.save(any(InUserPhoto.class))).thenReturn(new InUserPhoto().setId(1L));
         reportPhotoService.create("1", new PhotoRequestDTO().setGoal_id(1L).setDataurl(
                 "data:image/gif;base64,R0lGODlhEAAOALMAAOazToeHh0tLS/7LZv/0j"
+                + "vb29t/f3//Ub//ge8WSLf/rhf/3kdbW1mxsbP//mf///yH5BAAAAAAALAAAAAAQAA4AAA"
+                + "Re8L1Ekyky67QZ1hLnjM5UUde0ECwLJoExKcppV0aCcGCmTIHEIUEqjgaORCMxIC6e0Cc"
+                + "guWw6aFjsVMkkIr7g77ZKPJjPZqIyd7sJAgVGoEGv2xsBxqNgYPj/gAwXEQA7"));
+        verify(userService).checkUserToken(eq("1"));
+    }
+
+    @Test(expected = UnauthorizedException.class)
+    public void create_invalid_dataurl() {
+        InUserLogin inUserLogin = new InUserLogin();
+        InUser inUserForLogin = new InUser().setInUserGoals(Arrays.asList(new InUserGoal().setGoalId(1L)));
+        inUserForLogin.setId(10L);
+        inUserLogin.setInUser(inUserForLogin);
+        when(userService.checkUserToken(eq("1"))).thenReturn(inUserLogin);
+        doAnswer((Answer<Void>) (InvocationOnMock invocation) -> {
+            Object[] args = invocation.getArguments();
+            ((Errors) args[1]).reject("dataurlValidator", "dataurlValidator");
+            return null;
+        }).when(dataurlValidator).validate(anyObject(), any(Errors.class));
+        reportPhotoService.create("1", new PhotoRequestDTO().setGoal_id(1L).setDataurl(
+                "data:image/gif;base64,R0lGODlhEAAOALMAAOazToeHh0tLS/7LZv/0j"
+                + "vb29t/f3//Ub//ge8WSLf/rhf/3kdbW1mxsbP//mf///yH5BAAAAAAALAAAAAAQAA4AAA"
+                + "Re8L1Ekyky67QZ1hLnjM5UUde0ECwLJoExKcppV0aCcGCmTIHEIUEqjgaORCMxIC6e0Cc"
+                + "guWw6aFjsVMkkIr7g77ZKPJjPZqIyd7sJAgVGoEGv2xsBxqNgYPj/gAwXEQA7"));
+    }
+
+    @Test
+    public void create_photo_png() {
+        InUserLogin inUserLogin = new InUserLogin();
+        InUser inUserForLogin = new InUser().setInUserGoals(Arrays.asList(new InUserGoal().setGoalId(1L)));
+        inUserForLogin.setId(10L);
+        inUserLogin.setInUser(inUserForLogin);
+        when(userService.checkUserToken(eq("1"))).thenReturn(inUserLogin);
+        when(inUserPhotoRepository.save(any(InUserPhoto.class))).thenReturn(new InUserPhoto().setId(1L));
+        reportPhotoService.create("1", new PhotoRequestDTO().setGoal_id(1L).setDataurl(
+                "data:image/png;base64,R0lGODlhEAAOALMAAOazToeHh0tLS/7LZv/0j"
                 + "vb29t/f3//Ub//ge8WSLf/rhf/3kdbW1mxsbP//mf///yH5BAAAAAAALAAAAAAQAA4AAA"
                 + "Re8L1Ekyky67QZ1hLnjM5UUde0ECwLJoExKcppV0aCcGCmTIHEIUEqjgaORCMxIC6e0Cc"
                 + "guWw6aFjsVMkkIr7g77ZKPJjPZqIyd7sJAgVGoEGv2xsBxqNgYPj/gAwXEQA7"));
