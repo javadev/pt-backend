ec4621d2ba5d3d800f0e0031ff1368c87fd6936e
javadev <javadev75@gmail.com>
2018-10-13 00:01:30 +0300

    Fix sonar warnings

--- src/main/java/com/osomapps/pt/admin/user/AdminUserProgramFileService.java
+++ src/main/java/com/osomapps/pt/admin/user/AdminUserProgramFileService.java
@@ -29,22 +29,23 @@ class AdminUserProgramFileService {
         final InUser inUser = inUserRepository.findOne(id);
         if (inUser == null) {
             throw new ResourceNotFoundException("User with id " + id + " not found.");
-        }
-        FastByteArrayOutputStream localOutputStream = new FastByteArrayOutputStream();
-        final byte[] buffer = new byte[1024];
-        try (InputStream inputStream = XlsxProgramParser.class.getResourceAsStream("program01.xlsx")) {
+        }        
+        try (InputStream inputStream = XlsxProgramParser.class.getResourceAsStream("program01.xlsx");
+            FastByteArrayOutputStream localOutputStream = new FastByteArrayOutputStream()) {
+            final byte[] buffer = new byte[1024];
             int length;
             while ((length = inputStream.read(buffer)) != -1) {
                 localOutputStream.write(buffer, 0, length);
             }
+            final XlsxProgramModifier xlsxProgramModifier = XlsxProgramModifier.of(
+                    localOutputStream.getInputStream(), dictionaryService);
+            xlsxProgramModifier.updateCellData(outputStream, inUser);
+            return new ProgramResponseDTO().setFileName("program_for_user_" + id + ".xlsx")
+                    .setFileType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
         } catch (IOException ex) {
             log.error(ex.getMessage(), ex);
+            throw new UnsupportedOperationException(ex);
         }
-        final XlsxProgramModifier xlsxProgramModifier = XlsxProgramModifier.of(
-                localOutputStream.getInputStream(), dictionaryService);
-        xlsxProgramModifier.updateCellData(outputStream, inUser);
-        return new ProgramResponseDTO().setFileName("program_for_user_" + id + ".xlsx")
-                .setFileType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
     }
 
 }
