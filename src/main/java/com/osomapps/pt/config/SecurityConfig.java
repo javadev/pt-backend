99c5e9033d44b3504aca99ea2f4d5ac7ead6d857
Valentyn Kolesnikov <javadev75@gmail.com>
2018-12-14 11:18:52 +0100

    Fix checkstyle warnings.

--- src/main/java/com/osomapps/pt/config/SecurityConfig.java
+++ src/main/java/com/osomapps/pt/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.osomapps.pt.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
--- src/main/java/com/osomapps/pt/reportworkout/InWorkoutItemRepository.java
+++ src/main/java/com/osomapps/pt/reportworkout/InWorkoutItemRepository.java
@@ -1,7 +1,6 @@
 package com.osomapps.pt.reportworkout;
 
 import com.osomapps.pt.programs.InWorkoutItem;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface InWorkoutItemRepository extends JpaRepository<InWorkoutItem, Long> {
